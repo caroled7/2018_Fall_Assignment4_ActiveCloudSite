bef191da72a6acc67e804403b590caf58e7ce777
caroled7 <43157784+caroled7@users.noreply.github.com>
2018-10-30 20:14:31 -0400

    Simplied version of App

--- IEXTrading/Controllers/HomeController.cs
+++ IEXTrading/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using IEXTrading.Infrastructure.IEXTradingHandler;
 using IEXTrading.Models;
-using IEXTrading.Models.ViewModel;
 using IEXTrading.DataAccess;
 using Newtonsoft.Json;
 
@@ -29,8 +28,8 @@ namespace MVCTemplate.Controllers
         /****
          * The Symbols action calls the GetSymbols method that returns a list of Companies.
          * This list of Companies is passed to the Symbols View.
-        ****/
-        public IActionResult Symbols()
+       
+         public IActionResult Symbols()
         {
             //Set ViewBag variable first
             ViewBag.dbSucessComp = 0;
@@ -42,28 +41,47 @@ namespace MVCTemplate.Controllers
 
             return View(companies);
         }
+         ****/
 
         /****
-         * The Chart action calls the GetChart method that returns 1 year's equities for the passed symbol.
-         * A ViewModel CompaniesEquities containing the list of companies, prices, volumes, avg price and volume.
-         * This ViewModel is passed to the Chart view.
-        ****/
-        public IActionResult Chart(string symbol)
+ * The Symbols action calls the GetSymbols method that returns a list of Companies.
+ * This list of Companies is passed to the Symbols View.
+****/
+        public IActionResult Symbols() /*test new -- I want to add table count --*/
         {
-            //Set ViewBag variable first
-            ViewBag.dbSuccessChart = 0;
-            List<Equity> equities = new List<Equity>();
-            if (symbol != null)
+            int startrange = 0;
+            int rangecount = 10;
+            int tablecount = dbContext.Companies.Count();
+            if (tablecount != 0 )
             {
-                IEXHandler webHandler = new IEXHandler();
-                equities = webHandler.GetChart(symbol);
-                equities = equities.OrderBy(c => c.date).ToList(); //Make sure the data is in ascending order of date.
+                startrange = tablecount + 1;
+
             }
 
-            CompaniesEquities companiesEquities = getCompaniesEquitiesModel(equities);
+            //Set ViewBag variable first
+            ViewBag.dbSucessComp = 0;
+            IEXHandler webHandler = new IEXHandler();
+            List<Company> companies = webHandler.GetSymbols(startrange, rangecount);
 
-            return View(companiesEquities);
+            //Save comapnies in TempData
+            TempData["Companies"] = JsonConvert.SerializeObject(companies);
+
+            return View(companies);
+
+        
+
+
+
+
         }
+
+
+
+
+
+
+
+
 
         /****
          * The Refresh action calls the ClearTables method to delete records from a or all tables.
@@ -74,7 +92,6 @@ namespace MVCTemplate.Controllers
             ClearTables(tableToDel);
             Dictionary<string, int> tableCount = new Dictionary<string, int>();
             tableCount.Add("Companies", dbContext.Companies.Count());
-            tableCount.Add("Charts", dbContext.Equities.Count());
             return View(tableCount);
         }
 
@@ -98,30 +115,7 @@ namespace MVCTemplate.Controllers
             return View("Symbols", companies);
         }
 
-        /****
-         * Saves the equities in database.
-        ****/
-        public IActionResult SaveCharts(string symbol)
-        {
-            IEXHandler webHandler = new IEXHandler();
-            List<Equity> equities = webHandler.GetChart(symbol);
-            //List<Equity> equities = JsonConvert.DeserializeObject<List<Equity>>(TempData["Equities"].ToString());
-            foreach (Equity equity in equities)
-            {
-                if (dbContext.Equities.Where(c => c.date.Equals(equity.date)).Count() == 0)
-                {
-                    dbContext.Equities.Add(equity);
-                }
-            }
-
-            dbContext.SaveChanges();
-            ViewBag.dbSuccessChart = 1;
-
-            CompaniesEquities companiesEquities = getCompaniesEquitiesModel(equities);
-
-            return View("Chart", companiesEquities);
-        }
-
+       
         /****
          * Deletes the records from tables.
         ****/
@@ -130,43 +124,18 @@ namespace MVCTemplate.Controllers
             if ("all".Equals(tableToDel))
             {
                 //First remove equities and then the companies
-                dbContext.Equities.RemoveRange(dbContext.Equities);
                 dbContext.Companies.RemoveRange(dbContext.Companies);
             }
             else if ("Companies".Equals(tableToDel))
             {
                 //Remove only those that don't have Equity stored in the Equitites table
-                dbContext.Companies.RemoveRange(dbContext.Companies
-                                                         .Where(c => c.Equities.Count == 0)
-                                                                      );
+                dbContext.Companies.RemoveRange(dbContext.Companies  );
             }
-            else if ("Charts".Equals(tableToDel))
-            {
-                dbContext.Equities.RemoveRange(dbContext.Equities);
-            }
+
             dbContext.SaveChanges();
         }
 
-        /****
-         * Returns the ViewModel CompaniesEquities based on the data provided.
-         ****/
-        public CompaniesEquities getCompaniesEquitiesModel(List<Equity> equities)
-        {
-            List<Company> companies = dbContext.Companies.ToList();
 
-            if (equities.Count == 0)
-            {
-                return new CompaniesEquities(companies, null, "", "", "", 0, 0);
-            }
-
-            Equity current = equities.Last();
-            string dates = string.Join(",", equities.Select(e => e.date));
-            string prices = string.Join(",", equities.Select(e => e.high));
-            string volumes = string.Join(",", equities.Select(e => e.volume / 1000000)); //Divide vol by million
-            float avgprice = equities.Average(e => e.high);
-            double avgvol = equities.Average(e => e.volume) / 1000000; //Divide volume by million
-            return new CompaniesEquities(companies, equities.Last(), dates, prices, volumes, avgprice, avgvol);
-        }
 
     }
 }
